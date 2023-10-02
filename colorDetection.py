b0e89d0028115a20982e005767f4722588e06c08
IvanReyhan22 <ivanreyhan2002@gmail.com>
2023-10-02 15:17:27 +0700

    Combine small box & add color name

--- colorDetection.py
+++ colorDetection.py
@@ -29,9 +29,29 @@ mask = cv2.inRange(hsv_image, lower_color, upper_color)
 
 contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+combined_rectangles = []
+
 for contour in contours:
     x, y, w, h = cv2.boundingRect(contour)
+    area = cv2.contourArea(contour)
+    
+    # Gabung kotak dengan area yang sangat kecil
+    if area < 100:
+        continue
+    
+    # Gabung kotak yang berdekatan
+    merged = False
+    for rect in combined_rectangles:
+        if (x >= rect[0] and y >= rect[1] and x + w <= rect[0] + rect[2] and y + h <= rect[1] + rect[3]):
+            merged = True
+            break
+    if not merged:
+        combined_rectangles.append((x, y, w, h))
+
+for (x, y, w, h) in combined_rectangles:
     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    
+    cv2.putText(image, "kuning", (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 
 cv2.imshow('detection', image)
 
